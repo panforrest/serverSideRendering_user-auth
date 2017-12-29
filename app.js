01ea3fefd0d2db36191e9d8a4a9ed023060375de
panforrest <guoqianp@gmail.com>
2017-12-29 17:58:41 -0500

    14:00 my attempt to create new user to turbo backend

--- app.js
+++ app.js
@@ -7,10 +7,11 @@ const app = vertex.app()
 // import routes
 const index = require('./routes/index')
 const api = require('./routes/api')
+const user = require('./routes/user')
 
 // set routes
 app.use('/', index)
 app.use('/api', api) // sample API Routes
-
+app.use('/user', user)
 
 module.exports = app
---
+++ routes/user.js
@@ -0,0 +1,25 @@
+// Full Documentation - https://www.turbo360.co/docs
+const turbo = require('turbo360')({site_id: process.env.TURBO_APP_ID})
+const vertex = require('vertex360')({site_id: process.env.TURBO_APP_ID})
+const router = vertex.router()
+
+
+router.post('/register', function(req, res){
+	
+	turbo.createUser(req.body) //NOT query.body
+	.then(data => {
+		res.json({
+			confirmation: 'success',
+            data: data
+		})
+	})
+
+	.catch(err => {
+		res.json({
+			confirmation: 'fail',
+			message: err.message
+		})
+	})
+})
+
+module.exports = router
