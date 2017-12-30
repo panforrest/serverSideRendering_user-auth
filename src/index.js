6c580374e226de0707fd2b4fec2c6891f2553822
panforrest <guoqianp@gmail.com>
2017-12-29 23:29:44 -0500

    15:22 both cleint and server side render current user is TEST

--- src/index.js
+++ src/index.js
@@ -4,8 +4,10 @@ import store from './stores'
 import { Provider } from 'react-redux'
 import { Admin } from './components/containers'
 
+const initialState = window.__INITIAL_STATE__
+
 const app = (
-	<Provider store={store.configure(null)}>
+	<Provider store={store.configure(initialState)}>
 		<Admin />
 	</Provider>
 )
