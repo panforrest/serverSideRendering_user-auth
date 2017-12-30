a0ad63d135123acae35a55fb7adbf1352ea88b30
panforrest <guoqianp@gmail.com>
2017-12-29 22:58:28 -0500

    9:44 npm run build; html successful by checking terminal

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,25 @@ const turbo = require('turbo360')({site_id: process.env.TURBO_APP_ID})
 const vertex = require('vertex360')({site_id: process.env.TURBO_APP_ID})
 const router = vertex.router()
 
+const React = require('react')
+const ReactDOMServer = require('react-dom/server')
+
+//import our own React components
+const serverIndex = require('../public/dist/es5/server-index')
+const containers = require('../public/dist/es5/components/containers')
+const store = require('../public/dist/es5/stores')
 
 router.get('/', function(req, res){
+    const initialState = store.configure(null)
+
+	const admin = React.createElement(containers.Admin)
+	const entry = React.createElement(serverIndex, {component:admin, store:initialState})
+	const html = ReactDOMServer.renderToString(entry)
+	console.log('HTML: ' + html)
+
+// })
+
+// router.get('/', function(req, res){
 	res.render('index', {text: 'This is the dynamic data. Open index.js from the routes directory to see.'})
 })
 
